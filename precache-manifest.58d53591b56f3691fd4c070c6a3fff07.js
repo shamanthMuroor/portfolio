f25e6326d0e46de03360e93c197090e9841fa56c
ShamanthMuroor <shamanthm3@gmail.com>
2019-09-25 03:12:42 +0530

    Updates

---
+++ precache-manifest.58d53591b56f3691fd4c070c6a3fff07.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "556981abb406bec5a7522ba38406e504",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "569ee3859fc12069beb7",
+    "url": "/portfolio/static/css/main.4c8934a1.chunk.css"
+  },
+  {
+    "revision": "e582006eff383c0a66ac",
+    "url": "/portfolio/static/js/2.44f970da.chunk.js"
+  },
+  {
+    "revision": "569ee3859fc12069beb7",
+    "url": "/portfolio/static/js/main.43409c10.chunk.js"
+  },
+  {
+    "revision": "3bb6be1ea0653ce98535",
+    "url": "/portfolio/static/js/runtime-main.f69b92b3.js"
+  }
+]);
